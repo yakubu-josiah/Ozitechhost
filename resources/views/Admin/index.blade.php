34b9fa4ecd361c88b57c80e9e2a6e141ac7c61fd
yakubu-josiah <jossysmothest007@gmail.com>
2022-09-22 14:26:26 +0100

    skeleton of Admin view

---
+++ resources/views/Admin/index.blade.php
@@ -0,0 +1,29 @@
+@extends('Layout.layout')
+@section('title', 'Admin Only')
+
+
+@section('content')
+    <div>
+        <div>
+            <h3>Top Bar Navigation</h3>
+        </div>
+        <div>
+            <h3>The Second Navigation Bar</h3>
+        </div>
+        <div>
+            <h3>Slider Section Contents</h3>
+        </div>
+        <div>
+            <h3>Details On The Hosting Platform</h3>
+        </div>
+        <div>
+            <h3>The Subscription Plan Service</h3>
+        </div>
+        <div>
+            <h3>Brand Partners</h3>
+        </div>
+        <div>
+            <h3>Your Contact Detials On Footer</h3>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('home/index', function () {
+Route::get('/', function () {
     return view('home.index');
 })->name('home.index');
+Route::get('Admin/', function (){
+    return view('Admin.index');
+})->name('Admin.index');
