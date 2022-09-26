b115aa5e6c9f3e9ae414f46705d4c131726dbccb
yakubu-josiah <jossysmothest007@gmail.com>
2022-09-26 13:41:30 +0100

    Headbar Component

---
+++ resources/views/components/headbar.blade.php
@@ -0,0 +1,16 @@
+<div>
+    <div class="imgHome d-flex flex-column flex-md-row align-items-center p-3 px-md-4 border-bottom shadow-sm bg-info mb-5">
+        <a class="my-0 me-auto opacity-50 mx-5"  href="{{ route('home.index') }}"><img src="{{ asset('Images/logo.png') }}" width="170" height="40" alt="Our Logo"></a>
+        <nav class="my-2 my-me-0 me-3 "> 
+            <a href={{ route('home.index') }} >Home</a>
+            <a href={{ route('home.index') }} >Contact</a>
+            <a href={{ route('home.index') }} >About Us</a>
+        </nav>
+    </div>
+    <div class="mt-5">
+        {{$slot}}
+    </div>
+  
+</div>
+
+   
